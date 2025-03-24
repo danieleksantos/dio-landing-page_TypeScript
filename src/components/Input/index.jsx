d3f5aa4d51601e33e3f212099586afdfec310997
Daniele Karina dos Santos <danimistron@gmail.com>
2025-03-24 05:45:14 -0300

    add submit form

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,12 +1,18 @@
 import React from "react";
+import { Controller } from "react-hook-form";
 
 import { IconContainer, InputContainer, InputText } from "./styles";
 
-const Input = ({leftIcon, name, ...rest}) => {
+const Input = ({leftIcon, name, control, ...rest}) => {
     return(
         <InputContainer>
-        {leftIcon ? (<IconContainer>{leftIcon}</IconContainer>) : null}
-            <InputText {...rest}/>
+            {leftIcon ? (<IconContainer>{leftIcon}</IconContainer>) : null}
+            <Controller 
+            name={name}
+            control={control}
+            rules={{required: true}}
+            render={({field}) => <InputText {...field}{...rest}/>}
+            />                           
         </InputContainer>
     )
 }
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,14 +1,23 @@
-import {MdEmail, MdLock} from 'react-icons/md'
+import {MdEmail, MdLock} from 'react-icons/md';
 import { useNavigate } from 'react-router-dom';
-import { Button } from "../../components/Button"
-import { Header } from "../../components/Header"
-import { Input } from "../../components/Input"
-import {Column, Container, CriarText, EsqueciText, Row, SubTitleLogin, Title, TitleLogin, Wrapper } from "./styles"
+import { useForm } from "react-hook-form";
+import { Button } from "../../components/Button";
+import { Header } from "../../components/Header";
+import { Input } from "../../components/Input";
+import {Column, Container, CriarText, EsqueciText, Row, SubTitleLogin, Title, TitleLogin, Wrapper } from "./styles";
+import { CgPassword } from 'react-icons/cg';
 
 
 
 const Login = () => {
     const navigate = useNavigate();
+
+    const {
+        control,
+        handleSubmit,
+        formState: { errors, isValid },
+      } = useForm();
+      const onSubmit = data => console.log(data)
 
     const handleClickSignIn = () => {
         navigate('/feed')
@@ -29,10 +38,10 @@ const Login = () => {
                     <SubTitleLogin>
                         Faça seu login e make the change._
                     </SubTitleLogin>
-                    <form>
-                        <Input placeholder="E-mail" leftIcon={<MdEmail />}/>
-                        <Input placeholder="Senha" type="password" leftIcon={<MdLock />}/>
-                        <Button type="button" title="Entrar" variant="secundary" onClick={handleClickSignIn}/>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <Input name="email" control={control} placeholder="E-mail" leftIcon={<MdEmail />}/>
+                        <Input name="Password" control={control} placeholder="Senha" type="password" leftIcon={<MdLock />}/>
+                        <Button type="submit" title="Entrar" variant="secundary"/>
                     </form>
                     <Row>
                         <EsqueciText>
